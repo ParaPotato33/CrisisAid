82e0909dc16357b9b5cb041a2a452019f90e9a1f
ParaPotato33 <adamdylanrussell@gmail.com>
2024-04-27 14:39:11 +0100

    a

---
+++ MainPage/script.js
@@ -0,0 +1,16 @@
+function newCrisis(CrisisForm) {
+	
+	var country = CrisisForm.CountryInput.value;
+	CrisisForm.CountryInput.value = "";
+	var table = document.getElementById("crisisTable");
+	var row = table.insertRow();
+
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	const time = new Date();
+
+	cell1.innerHTML = time.getHours().toString().concat(":", time.getMinutes(), ":", time.getSeconds());
+	cell2.innerHTML = time.getDate().toString().concat("/", time.getMonth() + 1, "/", time.getFullYear());
+	cell3.innerHTML = country;
+}
