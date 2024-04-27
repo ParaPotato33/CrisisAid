171e18693d773c0dcc5ed52b01634d4dd3110890
ParaPotato33 <adamdylanrussell@gmail.com>
2024-04-27 20:39:42 +0100

    Update script.js

--- MainPage/script.js
+++ MainPage/script.js
@@ -2,7 +2,7 @@ async function pageLoad() {
     const response = await fetch("tableData.json");
     const savedData = await response.json();
 
-    alert(savedData);
+    alert(JSON.stringify(savedData));
 }
 
 function newCrisis(CrisisForm) {
